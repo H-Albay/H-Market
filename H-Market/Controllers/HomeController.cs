aadcfd489fdbc08379c63379c960e15d7be33277
Hakan Albay <hakanalbay2@gmail.com>
2018-11-27 22:20:56 +0300

    v0

---
+++ H-Market/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace H_Market.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Detials()
+        {
+            return View();
+        }
+        public ActionResult List()
+        {
+            return View();
+        }
+
+    }
+}
