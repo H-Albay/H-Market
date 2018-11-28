976bb85804862c8d1bf6661a14da93d7a4d12bad
Hakan Albay <hakanalbay2@gmail.com>
2018-11-28 12:12:15 +0300

    v7.1

---
+++ H-Market/Identity/IdentityDataContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace H_Market.Identity
+{
+    public class IdentityDataContext:IdentityDbContext<ApplicationUser>
+    {
+        public IdentityDataContext() : base("dataConnection")
+        {
+
+        }
+    }
+}
