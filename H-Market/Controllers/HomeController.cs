969c1fe54bb40ffe80b0d87d0e666210823cca6e
Hakan Albay <hakanalbay2@gmail.com>
2018-11-28 11:34:03 +0300

    v4.1

--- H-Market/Controllers/HomeController.cs
+++ H-Market/Controllers/HomeController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using H_Market.Entity;
+using H_Market.Models;
 
 namespace H_Market.Controllers
 {
@@ -11,17 +12,52 @@ namespace H_Market.Controllers
     {
         DataContext context = new DataContext();
         public ActionResult Index()
-        {   
-            return View(context.Products.Where(i=>i.IsHome && i.IsApproved).ToList());
+        {
+            var products = context.Products.Where(i => i.IsHome && i.IsApproved).Select(i => new ProductModel()
+            {
+                Id = i.Id,
+                Name = i.Name.Length > 50 ? i.Name.Substring(0, 47) + "..." : i.Name,
+                Desciption = i.Desciption.Length > 50 ? i.Desciption.Substring(0, 47) + "..." : i.Desciption,
+                Price = i.Price,
+                Stock = i.Stock,
+                Image = i.Image,
+                CategoryId = i.CategoryId
+
+            }).ToList();
+            return View(products);
         }
         public ActionResult Detials(int id)
         {
-            return View(context.Products.Where(i=>i.Id==id).FirstOrDefault());
+            return View(context.Products.Where(i => i.Id == id).FirstOrDefault());
         }
-        public ActionResult List()
+        public ActionResult List(int? id) //id parametrem RouteConfigdeki idden geliyor
         {
-            return View(context.Products.Where(i=>i.IsApproved).ToList());
+            var products = context.Products.Where(i => i.IsApproved).Select(i => new ProductModel()
+            {
+                Id = i.Id,
+                Name = i.Name.Length > 50 ? i.Name.Substring(0, 47) + "..." : i.Name,
+                Desciption = i.Desciption.Length > 50 ? i.Desciption.Substring(0, 47) + "..." : i.Desciption,
+                Price = i.Price,
+                Stock = i.Stock,
+                Image = i.Image ?? "1.jpg",
+                CategoryId = i.CategoryId
+
+            }).AsQueryable();
+
+            if (id != null) //Kategori id boş değil ise ilgili id getir.
+            {
+                products = products.Where(i => i.CategoryId == id);
+
+            }
+            return View(products.ToList());
         }
 
+
+        public PartialViewResult GetCategories()    //parçalı view oluşturulup istenildiği taktirde kullanıklabilir.
+        {
+            return PartialView(context.Categories.ToList());
+
+        }
     }
+    
 }
