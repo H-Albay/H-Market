f6b5e4d11b266022d628d5da76c2b63c6d3e1b6d
Hakan Albay <hakanalbay2@gmail.com>
2018-11-28 11:32:04 +0300

    v3.1

--- H-Market/Controllers/HomeController.cs
+++ H-Market/Controllers/HomeController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using H_Market.Entity;
+using H_Market.Models;
 
 namespace H_Market.Controllers
 {
@@ -11,8 +12,19 @@ namespace H_Market.Controllers
     {
         DataContext context = new DataContext();
         public ActionResult Index()
-        {   
-            return View(context.Products.Where(i=>i.IsHome && i.IsApproved).ToList());
+        {
+            var products = context.Products.Where(i => i.IsHome && i.IsApproved).Select(i => new ProductModel()
+            {
+                Id = i.Id,
+                Name = i.Name,
+                Desciption = i.Desciption.Length > 50 ? i.Desciption.Substring(0, 47) + "..." : i.Desciption,
+                Price = i.Price,
+                Stock=i.Stock,
+                Image=i.Image,
+                CategoryId=i.CategoryId
+
+            }).ToList();
+            return View(products);
         }
         public ActionResult Detials(int id)
         {
@@ -20,7 +32,18 @@ namespace H_Market.Controllers
         }
         public ActionResult List()
         {
-            return View(context.Products.Where(i=>i.IsApproved).ToList());
+            var products = context.Products.Where(i => i.IsApproved).Select(i => new ProductModel()
+            {
+                Id = i.Id,
+                Name = i.Name.Length>50 ? i.Name.Substring(0,47)+"...":i.Name,
+                Desciption = i.Desciption.Length > 50 ? i.Desciption.Substring(0, 47) + "..." : i.Desciption,
+                Price = i.Price,
+                Stock = i.Stock,
+                Image = i.Image ?? "1.jpg",
+                CategoryId = i.CategoryId
+
+            }).ToList();
+            return View(products);
         }
 
     }
---
+++ H-Market/Entity/DataInitializer.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace H_Market.Entity
+{
+    public class DataInitializer:DropCreateDatabaseIfModelChanges<DataContext>
+    {
+        protected override void Seed(DataContext context)
+        {
+            //List<Category> k = new List<Category>() yada
+            var categories = new List<Category>()
+            {
+                new Category(){ Name="Kamera",Description="Kamera Ürünleri"},
+                new Category(){ Name="Bilgisayar",Description="Bilgisayar Ürünleri"},
+                new Category(){ Name="Elektronik",Description="Elektronik Ürünleri"},
+                new Category(){ Name="Telefon",Description="Telefon Ürünleri"},
+                new Category(){ Name="Beyaz Eşya",Description="Beyaz Eşya Ürünleri"},
+
+            };
+
+            foreach (var i in categories)
+            {
+                context.Categories.Add(i);
+            }
+            context.SaveChanges();
+            
+            var products = new List<Product>()
+            {
+                new Product(){Name="SONY A6 16-50MM DiJiTAL FOTOĞRAF MAKiNESi SONY A6 16-50MM DiJiTAL FOTOĞRAF MAKiNESiSONY A6 16-50MM DiJiTAL FOTOĞRAF MAKiNESi",Desciption="24.3MP APS-C Exmor APS HD CMOS Sensör 24.3MP APS-C Exmor APS HD CMOS Sensör24.3MP APS-C Exmor APS HD CMOS Sensör24.3MP APS-C Exmor APS HD CMOS Sensör24.3MP APS-C Exmor APS HD CMOS Sensör24.3MP APS-C Exmor APS HD CMOS Sensör24.3MP APS-C Exmor APS HD CMOS Sensör24.3MP APS-C Exmor APS HD CMOS Sensör ",Price=250,Stock=9,IsApproved= true,CategoryId=1,IsHome=true,Image="1.jpg"},
+                new Product(){Name="MAKiNESi",Desciption="24.3MP APS-C Exmor APS HD CMOS Sensör",Price=350,Stock=500,IsApproved= true,CategoryId=1,Image="2.jpg"},
+                new Product(){Name="A6000",Desciption="24.3MP APS-C Exmor APS HD CMOS Sensör",Price=450,Stock=99,IsApproved= false,CategoryId=2,IsHome=true,Image="3.jpg"},
+                new Product(){Name="16-50MM",Desciption="24.3MP APS-C Exmor APS HD CMOS Sensör",Price=550,Stock=23,IsApproved= true,CategoryId=3,Image="2.jpg"},
+                new Product(){Name="DiJiTAL",Desciption="24.3MP APS-C Exmor APS HD CMOS Sensör",Price=650,Stock=55,IsApproved= false,CategoryId=4,IsHome=true,Image="1.jpg"},
+                new Product(){Name="FOTOĞRAF",Desciption="24.3MP APS-C Exmor APS HD CMOS Sensör",Price=750,Stock=41,IsApproved= false,CategoryId=5,Image="1.jpg"},
+                new Product(){Name="MAKiNESi",Desciption="24.3MP APS-C Exmor APS HD CMOS Sensör",Price=850,Stock=34,IsApproved= true,CategoryId=2,IsHome=true,Image="4.jpg"},
+                new Product(){Name="SSD",Desciption="24.3MP APS-C Exmor APS HD CMOS Sensör",Price=850,Stock=34,IsApproved= true,CategoryId=2,IsHome=true,Image="4.jpg"}
+
+            };
+
+            foreach (var i in products)
+            {
+                context.Products.Add(i);
+            }
+            context.SaveChanges();
+
+            base.Seed(context);
+        }
+    }
+}
